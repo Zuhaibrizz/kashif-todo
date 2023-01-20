ce4d3b4d575619aeee666fe3fb44fedc68f06992
Zuhaibrizz <zuhaibrizz@gmail.com>
2023-01-20 15:05:08 +0530

    Initial commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About {{$store.state.appTitle}}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus non, labore tempora repellendus
+      molestias enim at ratione, obcaecati numquam facilis voluptates, voluptatum vel amet. Enim quia odit nobis. Iusto
+      alias mollitia quibusdam distinctio, ad hic sed consequuntur praesentium beatae consectetur quod error voluptates
+      velit itaque ipsa enim eaque dicta quis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, dolores nihil temporibus, eveniet commodi
+      dolor, neque eligendi voluptas quo distinctio quas officiis eaque error! Ab ratione culpa pariatur optio
+      consequuntur, aspernatur eius. A dicta eaque tempore, numquam, iste, repudiandae debitis voluptas provident quidem
+      facere voluptatum eveniet? Esse cumque iusto quia!
+    </p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus tempore ipsum nostrum facere reprehenderit ab
+      maiores quas, vitae ad, dolor enim assumenda voluptate ea nisi unde corporis incidunt neque aspernatur. Ab eaque,
+      expedita modi ex recusandae quisquam sit maiores repellat veritatis illum reiciendis architecto necessitatibus
+      provident, sapiente excepturi. Corporis, incidunt!</p>
   </div>
 </template>
